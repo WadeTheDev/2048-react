299478c1c1d01a6cb450547e33bd972da1e9c7f3
WadeTheDev <echo1006j@gmail.com>
2022-07-26 13:34:27 +0200

    tiles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Grid from "./components/Grid";
 
+let init = 0
 class App extends React.Component {
   constructor(){
     super()
@@ -16,20 +17,19 @@ class App extends React.Component {
         ["","","",""],
         ["","","",""]
       ],
-      init: 0
     }
   }
 
-    reset = () => {
-      this.setState({grid: [
-        ["","","",""],
-        ["","","",""],
-        ["","","",""],
-        ["","","",""]
-      ]})
-    }
-
-    randomNumber = () => {
+  reset = () => {
+    this.setState({grid: [
+      ["","","",""],
+      ["","","",""],
+      ["","","",""],
+      ["","","",""]
+    ]})
+  }
+  
+  randomNumber = () => {
       let array = [2,4]
       let random = Math.floor(Math.random()* 2)
       let randomRow = Math.floor(Math.random()* 4)
@@ -37,23 +37,31 @@ class App extends React.Component {
       let gridClone = [...this.state.grid]
       if (gridClone[randomRow][randomColumn] === "") {
         gridClone[randomRow][randomColumn] =  array[random]
-        this.setState({grid: gridClone, init: this.state.init++})
-        if (this.state.init === 1) {
+        this.setState({grid: gridClone})
+        init++
+        if (init === 1) {
           this.randomNumber()
         }else{
-          this.setState({init: 0})
+          init = 0
         }
       }else(
         this.randomNumber()
-      )
-    }
+        )
+      }
+      
+      start = async () => {
+        const waitReset = await this.reset()
+        this.randomNumber()
+        console.log(waitReset)
+      }
 
-  start = async () => {
-    const waitReset = await this.reset()
-    this.randomNumber()
-  }
-  
-  render(){
+      left = () => {
+        this.state.grid.forEach((row) => {
+          
+        })
+      }
+      
+      render(){
     return (
       <>
         <section>
